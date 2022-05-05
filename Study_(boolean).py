8c094a51f8927f83b758c37429805ae37458e357
kiperkhm <53480313+kiperkhm@users.noreply.github.com>
2022-05-05 22:40:24 +0900

    Add files via upload

---
+++ Study_(boolean).py
@@ -0,0 +1,8 @@
+print(1<2)
+print(1>2)
+print(True)
+print(False)
+print(not 1>2)
+print(not 1<2)
+print(not False)
+print(not True)
---
+++ Study_(text-type).py
@@ -0,0 +1,3 @@
+print('다람쥐 헌 쳇바퀴에 타고파')
+print("hello world")
+print("helllo world "*5)
